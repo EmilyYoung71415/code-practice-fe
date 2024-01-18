76f29649dd431a6ed907fb9d6fc9af634cb3fa8e
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:41:19 +0800

    feat: asyncConcurrencyLimit2

--- src/async/async-concurrency-limit.test.ts
+++ src/async/async-concurrency-limit.test.ts
@@ -1,4 +1,4 @@
-import { asyncConcurrencyLimit } from './async-concurrency-limit';
+import { asyncConcurrencyLimit, asyncConcurrencyLimit2 } from './async-concurrency-limit';
 
 describe('异步函数并发limit', () => {
   async function mockAsyncFunc(data: number, timer: number) {
@@ -11,13 +11,23 @@ describe('异步函数并发limit', () => {
     });
   }
 
-  test('asyncPool', async () => {
+  test('asyncConcurrencyLimit1', async () => {
     const testArrData = Array.from({ length: 7 }, (_, index) => index);
     const promiseTasks = testArrData.map((data, index) => {
       return () => mockAsyncFunc(data, index);
     });
     const limitCount = 3;
     const result = await asyncConcurrencyLimit(promiseTasks, limitCount);
+    expect(result).toStrictEqual(testArrData);
+  });
+
+  test.only('asyncConcurrencyLimit2', async () => {
+    const testArrData = Array.from({ length: 7 }, (_, index) => index);
+    const promiseTasks = testArrData.map((data, index) => {
+      return () => mockAsyncFunc(data, index);
+    });
+    const limitCount = 3;
+    const result = await asyncConcurrencyLimit2(promiseTasks, limitCount);
     expect(result).toStrictEqual(testArrData);
   });
 });
--- src/async/async-concurrency-limit.ts
+++ src/async/async-concurrency-limit.ts
@@ -1,23 +1,72 @@
 // 实现这样一个函数，传入待执行的promise函数,并发数限制, 实现这样的并发执行函数
+
+import { resolve } from 'path';
+
 // asyncConcurrencyLimit(promiseTasks, limitCount).then(results => { console.log(results)});
 type AsyncTask<T> = () => Promise<T>;
 
+// 这个实现比较优雅，但比较不容易想到
+// 核心是：当这个数量达到时，我们使用 Promise.race() 等待其中一个任务完成，然后继续添加更多的任务，直到所有任务都被触发。
 export async function asyncConcurrencyLimit<T>(promiseTasks: AsyncTask<T>[], limitCount: number) {
   let activeTasks: Promise<T>[] = [];
   let results: Promise<T>[] = [];
 
   for (let i = 0; i < promiseTasks.length; i++) {
-    const curTaskPromise = promiseTasks[i](); // 先执行
-    activeTasks.push(curTaskPromise); // 把执行结果放在队列里
+    const curTaskPromise = promiseTasks[i](); // 立即调用执行
+    // 把执行中的promise放在队列里
+    activeTasks.push(curTaskPromise);
+    // 执行结果也是按顺序存放的
     results.push(curTaskPromise);
 
-    // 超出限制了or都执行了一遍了
+    // check是否已超出限制 or 已执行到末尾了
     if (activeTasks.length >= limitCount || i === promiseTasks.length - 1) {
+      // 等待至少一个任务完成
       await Promise.race(activeTasks);
-      // 移除已完成的任务
+      // activeTasks中移除已完成的任务
       activeTasks = activeTasks.filter(activePromise => activePromise !== curTaskPromise);
     }
   }
 
+  // 等待所有结果的返回
   return Promise.all(results);
 }
+
+// 这个是比较容易想到的
+export async function asyncConcurrencyLimit2<T>(promiseTasks: AsyncTask<T>[], limitCount: number) {
+  let activeCount = 0; // 当前执行中的，开始执行++， 执行完成--, 确保不会超过limit
+  let index = 0;
+  const results: T[] = [];
+
+  return new Promise((resolve, reject) => {
+    function runNext() {
+      if (index === promiseTasks.length && activeCount === 0) {
+        resolve(results);
+        return;
+      }
+
+      // 确保最多有limit个在执行
+      while (index < promiseTasks.length && activeCount < limitCount) {
+        activeCount++;
+        // 多声明一个curIndex是为了保证 results[curIndex]顺序记录结果的， cause .then的时候index不一定等于执行时的curIndex了
+        const curIndex = index;
+        promiseTasks[curIndex]()
+          .then(data => {
+            results[curIndex] = data;
+          })
+          .catch(err => {
+            reject(err);
+            // TODO: 可新增逻辑：失败后最大重试次数为retryCount
+          })
+          .finally(() => {
+            activeCount--;
+            runNext();
+          });
+        index++;
+      }
+    }
+
+    runNext();
+  });
+}
+
+// asyncConcurrencyLimit2.then(all Done).catch(err);
