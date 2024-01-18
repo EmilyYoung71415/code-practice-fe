4b66fe6edd9c1dbe1c2f310c6085756ed4425dc4
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:41:19 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+/// <reference types="vitest" />
+// Configure Vitest (https://vitest.dev/config/)
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'my-lib',
+      fileName: 'my-lib',
+    },
+  },
+  plugins: [dts()],
+});
